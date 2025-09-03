9d1a76309fa773101121d7222e8c537cdaea1607
v-s-v-i-s-h-w-a-s <vishwasvshanthkumar@gmail.com>
2025-09-03 23:54:19 +0530

    Fixed the Static analysis files

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,8 @@
-from src.app import app
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from app import app
 
 
 def test_home_route_status():
