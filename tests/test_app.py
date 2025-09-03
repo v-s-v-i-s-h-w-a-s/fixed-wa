57e08b7ad0d53e8e381059622efb23755a570e58
v-s-v-i-s-h-w-a-s <vishwasvshanthkumar@gmail.com>
2025-09-03 23:44:30 +0530

    Fixed the Static analysis files

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,10 @@
 from src.app import app
 
 
-
 def test_home_route_status():
     tester = app.test_client()
     response = tester.get("/")
     assert response.status_code == 200
-
 
 
 def test_home_route_text():
@@ -15,8 +13,6 @@ def test_home_route_text():
     assert b"Hello, World! from Flask" in response.data
 
 
-
 def test_home_route_content_type():
     tester = app.test_client()
-    response = tester.get("/")
-    assert response.content_type == "text/html; charset=utf-8"
+    assert tester.get("/").content_type == "text/html; charset=utf-8"
